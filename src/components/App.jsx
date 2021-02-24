d48440b7e2c45f360eed7d4aa5c9a37cc853b235
Jhonnar Rodriguez <jhonnar.rodriguez@gmail.com>
2021-02-24 18:30:36 -0500

    feat: add images support to webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import '../styles/index.scss';
 import Recipes from './Recipes';
+import payments from '../images/payments.png';
+import logoFooter from '../../public/assets/images/logo-footer.jpeg';
+import blueCake from '../../public/assets/images/blue-cake.svg';
 
 const App = () => {
   return (
@@ -10,6 +13,10 @@ const App = () => {
           <h1>Hi From App Updated!</h1>
         </section>
       </main>
+
+      <img src={payments} alt='Payment Image' />
+      <img src={logoFooter} alt='Logo Footer' />
+      <img src={blueCake} alt='Blue Cake' width='50px'/>
 
       <Recipes />
     </>
--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,29 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   mode,
   target,
+  output: {
+    assetModuleFilename: 'images/[hash][ext][query]',
+  },
   module: {
     rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset', // Webpack will check if the fille must generated inline (within the js file) or in a separate folder according to the max-size setting
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '',
+            }
+          },
+          'css-loader',
+          'postcss-loader', // The order matters because it will take the vender prefixes (original code) and put them into the source map
+          'sass-loader',
+        ]
+      },
       {
         test: /\.jsx?$/, // Everything that ends with .js
         exclude: /node_modules/, // We do not want to run the transpilation in stuff we won't need
@@ -20,15 +41,6 @@ module.exports = {
           loader: 'babel-loader',
         }
       },
-      {
-        test: /\.(s[ac]|c)ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader', // The order matters because it will take the vender prefixes (original code) and put them into the source map
-          'sass-loader',
-        ]
-      }
     ],
   },
   plugins: [
